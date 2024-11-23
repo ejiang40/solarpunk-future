2dec8fdefc8a6c8c6f87046d04ad5f4adadd25c8
Edwin Jiang <Edwin@lawn-10-91-105-71.lawn.gatech.edu>
2024-11-22 19:36:45 -0500

    Further fix coinfig

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/solarpunk-future/', // This should match your repository name exactly
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/repository-name',
+  base: '/solarpunk-future/', //repository name
 })
