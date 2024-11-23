4fca2bda2d09ff30c77522edb8c79fdf643a2573
Edwin Jiang <Edwin@lawn-10-91-105-71.lawn.gatech.edu>
2024-11-22 19:30:08 -0500

    Fix configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/solarpunk-future/', // This should match your repository name exactly
+})import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
